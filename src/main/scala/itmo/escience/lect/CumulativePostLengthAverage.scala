647a678551017e98af40526b4b321d321506fca3
Nikolay Butakov <alipoov.nb@gmail.com>
2017-05-19 15:38:10 +0300

    minor restructuring

--- src/main/scala/itmo/escience/lect/CumulativePostLengthAverage.scala
+++ src/main/scala/itmo/escience/lect/CumulativePostLengthAverage.scala
@@ -1,8 +1,14 @@
 package itmo.escience.lect
 
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 import org.apache.spark.util.AccumulatorV2
+import spray.json.{JsString, JsValue, RootJsonFormat, deserializationError}
 
-import scala.collection.mutable
+import scala.collection.{Map, mutable}
+
+import spray.json.DefaultJsonProtocol._
+import spray.json._
 
 /**
   * Created by nikolay on 19.05.17.
@@ -28,11 +34,108 @@ class CumulativePostLengthAverage private (averPostLengthByUsers: mutable.HashMa
   }
 
   override def merge(other: AccumulatorV2[(String, Int), mutable.HashMap[String, (Int, Int)]]): Unit = {
-    for ((uId, (sumlength_other, count_other)) <- other) {
+    for ((uId, (sumlength_other, count_other)) <- other.value) {
       val (sumlength, count) = averPostLengthByUsers.getOrElse(uId, (0, 0))
       averPostLengthByUsers.update(uId, (sumlength + sumlength_other, count + count_other))
     }
   }
 
   override def value: mutable.HashMap[String, (Int, Int)] = averPostLengthByUsers.clone()
+}
+
+case class User(_id: String, key: String)
+
+case class Post(_id: String, text: String, userId: String, userName: Option[String] = None)
+
+object JsonFormats {
+  val userJsonFormat = UserJsonFormat
+  val postJsonFormat = PostJsonFormat
+}
+
+object UserJsonFormat extends RootJsonFormat[User] {
+
+  def write(c: User): JsValue = {
+    throw new NotImplementedError()
+  }
+
+  def read(value: JsValue): User = {
+    value.asJsObject.getFields("_id", "key") match {
+      case Seq(JsString(id), JsString(key)) =>
+        User(id, key)
+      case _ => deserializationError("User expected")
+    }
+  }
+}
+
+
+object PostJsonFormat extends RootJsonFormat[Post] {
+
+  def write(c: Post): JsValue = {
+    throw new NotImplementedError()
+  }
+
+  def read(value: JsValue): Post = {
+    value.asJsObject.getFields("_id", "text", "user") match {
+      case Seq(JsString(id), JsString(text), user) =>
+        val userId = user.asJsObject.getFields("id_str").head.toString()
+        Post(id, text, userId)
+      case _ => deserializationError("Post expected")
+    }
+  }
+}
+
+object ProcessingFuncs {
+
+  def loadUsersFromJson(path: String)(implicit sc: SparkContext): RDD[User] = {
+      sc.textFile(path)
+        .filter(_.nonEmpty)
+        .map(_.parseJson.convertTo[User](JsonFormats.userJsonFormat))
+  }
+
+  def loadPostsFromJson(path: String)(implicit sc: SparkContext): RDD[Post] = {
+    sc.textFile(path).cache()
+      .filter(_.nonEmpty)
+      .map(_.parseJson.convertTo[Post](JsonFormats.postJsonFormat))
+  }
+
+  def calculateAveragePostSizePerUser(posts: RDD[Post]): Map[String, Double] = {
+    posts
+      .groupBy(_.userId)
+      .map({
+        case (userId, uposts) => (userId, uposts.map(_.text.length).sum.toDouble / uposts.size)
+      })
+      .collectAsMap()
+  }
+
+  def calculateAveragePostSizePerUser(posts: RDD[Post])(implicit sc: SparkContext): Map[String, Double] = {
+
+    val cumulAvr = new CumulativePostLengthAverage()
+
+    sc.register(cumulAvr)
+
+    posts.foreach { p =>
+      cumulAvr.add(p.userId -> p.text.length)
+    }
+
+    cumulAvr.value.map { case (uId, (sumlength, count)) => uId -> sumlength.toDouble / count}.toMap
+  }
+
+  def broadcastingOfUsers(users: RDD[User], posts: RDD[Post])(implicit sc: SparkContext): RDD[Post] = {
+
+    val userNames = users.map(u => u._id -> u.key).distinct().collect().toMap
+
+    val bUserNames = sc.broadcast(userNames)
+
+    posts.map(p => Post( p._id, p.text, p.userId,
+      Option(bUserNames.value.getOrElse(p.userId, null)))
+    )
+  }
+
+  def byJoiningWithUsers(users: RDD[User], posts: RDD[Post])(implicit sc: SparkContext): RDD[Post] = {
+    val mappedPosts = posts.map(p => p.userId -> p)
+    val mappedUsers = users.map(u => u._id -> u)
+
+    mappedPosts.join(mappedUsers).map { case (uId, (p, u)) => Post(p._id, p.text, p.userId, Option(u.key) )}
+  }
+
 }
--- src/main/scala/itmo/escience/lect/MyApp.scala
+++ src/main/scala/itmo/escience/lect/MyApp.scala
@@ -1,11 +1,7 @@
 package itmo.escience.lect
 
-import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
-import spray.json.DefaultJsonProtocol._
-import spray.json._
-
-import scala.collection._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 import scala.util.Random
 
@@ -13,123 +9,34 @@ object MyApp {
 
   def main(args: Array[String]): Unit = {
 
-    val sc = initSpark()
+    val spark = initSpark()
+    implicit val sc = spark.sparkContext
 
-    val users =
-      sc.textFile("/mnt/share133/data-lect/Trump/users-subset.json")
-        .filter(_.nonEmpty)
-        .map(_.parseJson.convertTo[User](JsonFormats.userJsonFormat))
-        .cache()
+    val users = ProcessingFuncs.loadUsersFromJson("/mnt/share133/data-lect/Trump/users-subset.json").cache()
 
+    val posts = ProcessingFuncs.loadUsersFromJson("/mnt/share133/data-lect/Trump/users_posts-subset.json").cache()
 
-    val posts =
-      sc.textFile("/mnt/share133/data-lect/Trump/users_posts-subset.json").cache()
-      .filter(_.nonEmpty)
-      .map(_.parseJson.convertTo[Post](JsonFormats.postJsonFormat))
-      .cache()
+//    "/mnt/share133/data-lect/Trump/users_posts-subset-2.json"
 
-    val posts2 =
-      sc.textFile("/mnt/share133/data-lect/Trump/users_posts-subset-2.json").cache()
-        .filter(_.nonEmpty)
-        .map(_.parseJson.convertTo[Post](JsonFormats.postJsonFormat))
-        .cache()
-
-    calculateAveragePostSizePerUser(posts)
-
-    println(users)
-    println(posts)
+//    calculateAveragePostSizePerUser(posts)
+//
+//    val fullPosts = byJoiningWithUsers(sc, users, posts)
+//
+//    fullPosts.toDF().write.save("data.parquet")
   }
 
-  private def initSpark(): SparkContext = {
+  private def initSpark(): SparkSession = {
     val master = "local[4]"
     var sparkConf = new SparkConf()
       .setMaster(master)
       .setAppName(s"SparkApp_${new Random().nextInt(10000)}")
       .set("spark.network.timeout", "6000s")
-    val sc = new SparkContext(sparkConf)
-    sc
-  }
 
-  private def calculateAveragePostSizePerUser(posts: RDD[Post]): Map[String, Double] = {
-    posts
-      .groupBy(_.userId)
-      .map({
-        case (userId, uposts) => (userId, uposts.map(_.text.length).sum.toDouble / uposts.size)
-      })
-      .collectAsMap()
-  }
+    val spark = SparkSession
+      .builder()
+      .config(sparkConf)
+      .getOrCreate()
 
-  private def calculateAveragePostSizePerUser(sc: SparkContext, posts: RDD[Post]): Map[String, Double] = {
-
-    val cumulAvr = new CumulativePostLengthAverage()
-
-    sc.register(cumulAvr)
-
-    posts.foreach { p =>
-      cumulAvr.add(p.userId -> p.text.length)
-    }
-
-    cumulAvr.value.map { case (uId, (sumlength, count)) => uId -> sumlength.toDouble / count}.toMap
-  }
-
-  private def broadcastingOfUsers(sc: SparkContext, users: RDD[User], posts: RDD[Post]): RDD[Post] = {
-
-    val userNames = users.map(u => u._id -> u.key).distinct().collect().toMap
-
-    val bUserNames = sc.broadcast(userNames)
-
-    posts.map(p => Post( p._id, p.text, p.userId,
-      Option(bUserNames.value.getOrElse(p.userId, null)))
-    )
-  }
-
-  private def byJoiningWithUsers(sc: SparkContext, users: RDD[User], posts: RDD[Post]): RDD[Post] = {
-    val mappedPosts = posts.map(p => p.userId -> p)
-    val mappedUsers = users.map(u => u._id -> u)
-
-    mappedPosts.join(mappedUsers).map { case (uId, (p, u)) => Post(p._id, p.text, p.userId, Option(u.key) )}
-  }
-
-
-}
-
-case class User(_id: String, key: String)
-
-case class Post(_id: String, text: String, userId: String, userName: Option[String] = None)
-
-object JsonFormats {
-  val userJsonFormat = UserJsonFormat
-  val postJsonFormat = PostJsonFormat
-}
-
-object UserJsonFormat extends RootJsonFormat[User] {
-
-  def write(c: User): JsValue = {
-    throw new NotImplementedError()
-  }
-
-  def read(value: JsValue): User = {
-    value.asJsObject.getFields("_id", "key") match {
-      case Seq(JsString(id), JsString(key)) =>
-        User(id, key)
-      case _ => deserializationError("User expected")
-    }
-  }
-}
-
-
-object PostJsonFormat extends RootJsonFormat[Post] {
-
-  def write(c: Post): JsValue = {
-    throw new NotImplementedError()
-  }
-
-  def read(value: JsValue): Post = {
-    value.asJsObject.getFields("_id", "text", "user") match {
-      case Seq(JsString(id), JsString(text), user) =>
-        val userId = user.asJsObject.getFields("id_str").head.toString()
-        Post(id, text, userId)
-      case _ => deserializationError("Post expected")
-    }
+    spark
   }
 }
